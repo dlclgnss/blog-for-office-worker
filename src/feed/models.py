8c1c685ddf5610d5756da3371b276c752edbf985
Chihonn Lee <dlclgnss@gmail.com>
2019-10-10 17:56:06 +0900

    20191010_hashtag,category

--- src/feed/models.py
+++ src/feed/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Hashtag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     DEVELOPMENT = 'dv'
     PERSONAL = 'ps'
@@ -8,7 +15,7 @@ class Article(models.Model):
         (PERSONAL, "personal")
     )
 
-    title=models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     category = models.CharField(
         max_length=2,
@@ -16,25 +23,20 @@ class Article(models.Model):
         default= DEVELOPMENT,
     )
 
+    #hashtagf를 사용하려면 hashtag class가 위에 있어야 한다.
+    hashtag = models.ManyToManyField(Hashtag)
+
     def __str__(self):
         return self.title
 
     def summary(self):
-        return self.content[:50]
+        return self.content[:200]
 
 
 class Comment(models.Model):
-    article=models.ForeignKey(Article,on_delete=models.CASCADE)
+    article= models.ForeignKey(Article,on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     content = models.CharField(max_length=200)
 
     def __str__(self):
         return '{}의 대한 댓글:{}'.format(self.article.title, self.content)
-
-
-
-class Hashtag(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
--- src/feed/views.py
+++ src/feed/views.py
@@ -3,19 +3,42 @@ from .models import Article,Comment,Hashtag
 
 
 def index(request):
-    article_list = Article.objects.all()
+
+    category = request.GET.get("category")
+    hashtag = request.GET.get("hashtag")
+
     hashtag_list = Hashtag.objects.all()
+    if not category and not hashtag:
+        article_list = Article.objects.all()
+    elif category:
+        article_list = Article.objects.filter(category = category)
+    else:
+        article_list = Article.objects.filter(hashtag__name = hashtag) #model에 hashtag의name을 가져와서 집어넣는다.
+
+
+    # category_list = set([])
+    # for article in article_list:
+    #     category_list.add(article.get_category_display())
+    # 같은 내용인데 문법으로 짧게코딩 ↓#
+    category_list = set([
+    (article.category,article.get_category_display()) #튜플모양으로 전달, get_category_displays는 'dv','ps'를 뜻함
+    for article in article_list
+    ])
+
     ctx = {
         "article_list":article_list,
-        "hashtag_list":hashtag_list
+        "hashtag_list":hashtag_list,
+        "category_list":category_list
     }
     return render(request,'index.html',ctx)
 
 
 def detail(request,article_id):
     article = Article.objects.get(id = article_id )
+    hashtag_list = Hashtag.objects.all()
     ctx={
-        'article':article
+        'article':article,
+        "hashtag_list":hashtag_list,
     }
     return render(request,'detail.html',ctx)
 
